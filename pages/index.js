18917e68f65132cb7da487c5be3ff0fa71c59df8
SeongHunGit <justfix010@gmail.com>
2022-08-06 20:55:07 +0900

    #1.3 Routing

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-// react를 import해 올 필요는 없지만, react method를 쓰고 싶다면 react를 import 해야한다.
+import NavBar from "../components/NavBar";
+
 export default function Home() {
   return (
     <div>
-      <h1>hello</h1>
+      <NavBar />
+      <h1>Home</h1>
     </div>
   )
 }
