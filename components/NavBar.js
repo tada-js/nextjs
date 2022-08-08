a916d2b79708aaae4b05493bbef717cd3f046881
SeongHunGit <justfix010@gmail.com>
2022-08-08 12:19:29 +0900

    #1.5 Styles JSX

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import styles from "./NavBar.module.css"
 
-// module 사용은 파일을 바꿔줘야 하고 클래스 이름들을 기억해줘야 된다. 그리고 클래스이름 자체를 구현하는게 꽤 복잡하다.
 export default function NavBar() {
   const router = useRouter();
 
@@ -10,14 +8,22 @@ export default function NavBar() {
     <>
       <nav>
       <Link href="/"> 
-        <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : ""
-        ].join(" ")}>About</a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      <style jsx>{`
+      nav {
+        background-color: tomato;
+      }
+      a {
+        text-decoration: none;
+      }
+      .active {
+        color: yellow;
+      }
+      `}</style>
       </nav>
     </>
     )
