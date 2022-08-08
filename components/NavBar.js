7e3fce7cc1320eb7c6133ed9c1bc4c397f9d2b1c
SeongHunGit <justfix010@gmail.com>
2022-08-08 12:04:13 +0900

    #1.4 CSS Modules

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,24 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import styles from "./NavBar.module.css"
 
+// module 사용은 파일을 바꿔줘야 하고 클래스 이름들을 기억해줘야 된다. 그리고 클래스이름 자체를 구현하는게 꽤 복잡하다.
 export default function NavBar() {
   const router = useRouter();
 
   return (
     <>
-      <Link href="/">
-        <a style={{color: router.pathname === "/" ? "red" : "blue"}}>Home</a>
+      <nav>
+      <Link href="/"> 
+        <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
       </Link>
       <Link href="/about">
-        <a style={{color: router.pathname === "/about" ? "red" : "blue"}}>About</a>
+        <a className={[
+          styles.link,
+          router.pathname === "/about" ? styles.active : ""
+        ].join(" ")}>About</a>
       </Link>
+      </nav>
     </>
     )
 }
