53dd84505017bdc715e97901f40e3cd005c43aad
Kim Seong Hun <justfix010@gmail.com>
2022-08-17 20:11:12 +0900

    #2.5 Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,51 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "5d49c9ffed6dfb47bdaffe5df114d183"
-
-export default function Home() {
-  const [movies, setMovies] = useState([])
-  useEffect(() => {
-    (async () => {
-      const {results} = await (
-        await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json()
-        setMovies(results)
-    })()
-  }, [])
+export default function Home({ results }) {
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map(movie => (
-        <div key={movie.id}>
+      {results?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
-        )
-      )}
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
